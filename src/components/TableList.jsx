c951fd125372677ca1e20c953f53ebff9b5155d4
Busayo Samuel <busayosamuel2016@gmail.com>
2021-12-07 22:03:51 +0100

    debugging a production error

--- src/components/TableList.jsx
+++ src/components/TableList.jsx
@@ -30,8 +30,8 @@ export default function TableList({ loading }) {
   if (loading || !state.productList) {
     console.log('that happened')
     return (
-      <div className="mx-auto mt-32">
-        <LoadingSpinner height={"32"} width={"32"} color={"green-800"} />
+      <div className="mx-auto mt-32 text-9xl">
+       loading
       </div>
     );
   }
